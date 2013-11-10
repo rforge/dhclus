27f64fc31db7a741082a96d09e4f363cc95b4c1b
evidal <evidal@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-10 19:45:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dhclus@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- icav/R/withinSum.R
+++
@@ -1,8 +0,0 @@
-withinSum<-function(Data,labels,Distance=FALSE){
-Data<-t(Data)
-if(!Distance)DMAT<-.Call("icav_sq_euc_metric",Data, c(dim(Data)[1],dim(Data)[2]),DUP=F)
-else DMAT<-Data
-if (ncol(DMAT)!=length(labels)) stop("Data length and labels length do not match")
-W<-.Call("icav_gap_withinSum",DMAT,as.integer(labels),unique(labels),length(labels),DUP=F)
-return(W)
-}
--- pknng/src/strct.h
+++
@@ -1,6 +0,0 @@
-typedef struct c_node{
-	int id;			//ID number of vertex
-	void *strt;		// here you put what ever you want....
-	struct c_node *alpha;	//alpha and beta is for up and down or left ans rigth 
-	struct c_node *beta;	//
-} c_node;
