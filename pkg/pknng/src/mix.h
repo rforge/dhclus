9ea4a58beb52f6ad757f682b26c857d57af85fc5
evidal <evidal@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-11 11:53:52 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dhclus@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/pknng/src/mix.h
@@ -0,0 +1,8 @@
+extern void mi_metric_cfun (double *, int *, double *);
+extern SEXP correlation_metric(SEXP , SEXP );
+extern SEXP euc_metric(SEXP , SEXP );
+extern SEXP histo_fun(SEXP , SEXP );
+extern SEXP mi_metric(SEXP , SEXP );
+extern SEXP mi_estimator(SEXP , SEXP );
+
+
