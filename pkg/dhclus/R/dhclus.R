3558c720a6f4daea630adfacdf727b41bf2ae3a8
evidal <evidal@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-06 12:46:55 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dhclus@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dhclus/R/dhclus.R
+++ pkg/dhclus/R/dhclus.R
@@ -17,7 +17,7 @@ dhclus<- function(data, diss=FALSE,debug=FALSE,FUNCluster=Get.clusters, FUNTest=
   vec<-as.matrix(class$tags)
   max<-(max_level(class$tags))
   if(max >0){
-    <-max
+    i<-max
     t<-class$tags
     while(i > 1) 
     { 
