968e5f5849453532c745d1fce2e731a345488e3b
evidal <evidal@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-21 20:20:10 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dhclus@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/pknng/tests/test.ring.highNoise.R
@@ -0,0 +1,13 @@
+library(pknng)
+data(Three.Rings.HighNoise)
+
+d.iso.a<-pknng(data[,-3],3,diss=FALSE,mu="mean",method="euclidean",conn="one",penalize=1)
+HCmethod<-"average"
+
+a<-hclust(as.dist(d.iso.a),method = HCmethod)
+label.a<-cutree(a,k=5)
+plot(data[,1],data[,2],col=data[,3],pch=16,cex=0.5)
+title("True labels")
+
+plot(data[,1],data[,2],col=label.a,pch=16,cex=0.5)
+title("Clustering Result")
